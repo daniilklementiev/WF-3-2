a23cd34a065c5a2abc80779463a10b56fa1275f0
Klementiev Daniil <Klem_em31@student.itstep.org>
2022-09-02 13:39:32 +0400

    wasd+

--- Les2/Form1.cs
+++ Les2/Form1.cs
@@ -14,19 +14,19 @@ namespace Les2
         {
             Point? newLoc = null!;
 
-            if (e.KeyCode == Keys.Up)
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
             {
                 newLoc = new Point(this.Location.X, this.Location.Y - 50);
             }
-            else if (e.KeyCode == Keys.Down)
+            else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
             {
                 newLoc = new Point(this.Location.X, this.Location.Y + 50);
             }
-            else if (e.KeyCode == Keys.Left)
+            else if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
             {
                 newLoc = new Point(this.Location.X - 50, this.Location.Y);
             }
-            else if (e.KeyCode == Keys.Right)
+            else if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
             {
                 newLoc = new Point(this.Location.X + 50, this.Location.Y);
             }
