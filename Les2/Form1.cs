688f5c6c0b9c8ee92b0c4239e18c4a68e5845cf6
Klementiev Daniil <Klem_em31@student.itstep.org>
2022-09-02 15:31:58 +0400

    buttons

--- Les2/Form1.cs
+++ Les2/Form1.cs
@@ -1,27 +1,57 @@
 
+using System;
+
 namespace Les2
 {
     public partial class Form1 : Form
     {
         DateTime timeStarted;
-        
+        private const int M = 5;
+        private const int N = 5;
+        private int X = 50;
+        private int Y = 20;
+        Random rnd = new Random();
+        int counter;
+        Button[,] buttons = new Button[M, N];
         public Form1()
         {
             InitializeComponent();
-            
+            // проинициализировать массив кнопок
+            counter = 0;
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Text = counter++.ToString();
+                    buttons[i, j].Parent = this;
+                    buttons[i, j].Height = 75;
+                    buttons[i, j].Width = 75;
+                    buttons[i, j].BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                    counter++;
+                    buttons[i, j].Text = counter.ToString();
+                    buttons[i, j].Click += new EventHandler(ButtonRemoving);
+                    buttons[i, j].Location = new Point(i * X + i * Y, j * X + j * Y);
+                }
+            }
         }
         
-        
+        private void ButtonRemoving(object sender, EventArgs e)
+        {
+            Controls.Remove((Button)sender);
+        }
+
+
         private void timer1_Tick(object sender, EventArgs e)
         {
             this.Text = (DateTime.Now - timeStarted).Milliseconds.ToString();
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             timer1.Start();
             timeStarted = DateTime.Now;
+           
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
